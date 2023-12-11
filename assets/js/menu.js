458eca001a83a8269359293f8e8c42f0a846c4ef
Markpm-code <marlonseglos@yahoo.com>
2023-12-11 00:04:02 +0000

    added gradient in menu template

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,3 +2,4 @@ function showAllergens(foodItem) {
     const allergensDiv = document.getElementById(`allergens-${foodItem}`);
     allergensDiv.style.display = allergensDiv.style.display === 'none' ? 'block' : 'none';
   }
+
