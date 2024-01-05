f419b28b49448f6429296d4305f23c663857ebc4
Markpm-code <marlonseglos@yahoo.com>
2024-01-05 12:27:29 +0000

    script update

--- assets/js/scrolling_text.js
+++ assets/js/scrolling_text.js
@@ -2,5 +2,5 @@
 const paragraphs = document.querySelectorAll('.scrolling-text p');
 
 paragraphs.forEach((p, index) => {
-    p.style.animationDuration = ((p.textContent.length / 25) + index) + 's';
+    p.style.animationDuration = ((p.textContent.length / 25) + index) + 'm';
 });
