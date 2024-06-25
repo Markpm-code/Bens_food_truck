d1f9e1c205bc9b523684be068da48beddea2c633
Markpm-code <marlonseglos@yahoo.com>
2024-06-25 07:14:05 +0000

    wrap scrolling text with an anchor element and remove unused script

--- assets/js/scrolling_text.js
+++
@@ -1,6 +0,0 @@
-//scrolling text
-const paragraphs = document.querySelectorAll('.scrolling-text p');
-
-paragraphs.forEach((p, index) => {
-    p.style.animationDuration = ((p.textContent.length / 25) + index) + 'm';
-});
